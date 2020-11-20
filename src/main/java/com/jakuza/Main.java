a16e52d724cfbd340c71779e261e2265db427e4c
Zoltan Jakubecz <jakuzo@gmail.com>
2020-11-20 08:33:29 +0100

    use required json object

--- src/main/java/com/jakuza/Main.java
+++ src/main/java/com/jakuza/Main.java
@@ -12,17 +12,25 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        JSONObject json = new JSONObject();
-        json.put("fullName", "Kakket Makker Laller");
-        CloseableHttpClient client = HttpClients.createDefault();
-        HttpPost httpPost = new HttpPost("http://localhost:8080");
+        JSONObject userJson = new JSONObject();
+        userJson.put("fullName", "John Smith");
+        JSONObject applicationJson = new JSONObject();
+        applicationJson.put("name", "Need For Speed");
 
-        StringEntity entity = new StringEntity(json.toString());
+        JSONObject reqJson = new JSONObject();
+        reqJson.put("appUser", userJson);
+        reqJson.put("application", applicationJson);
+
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpPost httpPost = new HttpPost("http://localhost:8080/system/basic");
+
+        StringEntity entity = new StringEntity(userJson.toString());
         httpPost.setEntity(entity);
         httpPost.setHeader("Accept", "application/json");
         httpPost.setHeader("Content-type", "application/json");
 
         CloseableHttpResponse response = client.execute(httpPost);
+        System.out.println(response);
         client.close();
     }
 
