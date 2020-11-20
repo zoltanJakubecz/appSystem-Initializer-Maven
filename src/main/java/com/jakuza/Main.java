40933cca51a1bab9616a14f255c70043d5b17479
Zoltan Jakubecz <jakuzo@gmail.com>
2020-11-20 06:22:22 +0100

    use JSONObject

--- src/main/java/com/jakuza/Main.java
+++ src/main/java/com/jakuza/Main.java
@@ -5,19 +5,19 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.json.JSONObject;
 
 import java.io.IOException;
 
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        JSONObject json = new JSONObject();
+        json.put("fullName", "Kakket Makker Laller");
         CloseableHttpClient client = HttpClients.createDefault();
         HttpPost httpPost = new HttpPost("http://localhost:8080");
 
-        String json =  "{" +
-                "\"fullName\": \"Jaffa Elemer\" " +
-                "}";
-        StringEntity entity = new StringEntity(json);
+        StringEntity entity = new StringEntity(json.toString());
         httpPost.setEntity(entity);
         httpPost.setHeader("Accept", "application/json");
         httpPost.setHeader("Content-type", "application/json");
